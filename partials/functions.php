5a9ebcecab77eae77c255ef4dca8aef2879db3e4
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-11 18:05:42 +0200

    try for milestone 4

--- partials/functions.php
+++ partials/functions.php
@@ -22,35 +22,30 @@ function generate_password($password_length)
 
 /* 
 <?php
-session_start();
 
-if (isset($password_length) && $password_length !== '') {
 
-    $password = generate_password($password_length, $number, $letters, $symbol);
-
-    $_SESSION["password"] = $password;
-
-    //var_dump($_SESSION["password"]);
-};
-
-function generate_password($password_length, $number, $letters, $symbol)
+function generate_password($password_length, $numbers, $letters, $symbols)
 {
     if ($password_length > 0 && $password_length < 30) {
 
-        if ($number !== null) {
-            $number = '0123456789';
+        $numbers = '0123456789';
+        $letters = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXY';
+        $symbols = '.-_?!/+<>^][@#)(/&%$£*°ç={}\'/|:,;"~';
+        $characters = '';
+
+        if ($numbers !== null) {
+            $characters .= $numbers;
         }
 
         if ($letters !== null) {
-            $letters = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXY';
+            $characters .= $letters;
         }
 
-        if ($symbol !== null) {
-            $symbol = '.-_?!/+<>^][';
+        if ($symbols !== null) {
+            $characters .= $symbols;
         }
 
         //$characters = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789.-_?!/+<>^][";
-        $characters = $number . $letters . $symbol;
 
         for ($i = 0; $i < $password_length; $i++) {
 
@@ -61,4 +56,4 @@ function generate_password($password_length, $number, $letters, $symbol)
     }
 
     return (implode('', $new_password));
-}; */
+}*/
